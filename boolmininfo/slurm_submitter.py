4874fa223c7c8880deb00682824b6caf7c545daa
Patrick Wall <patgwall@elogin2.bigred3.uits.iu.edu>
2021-01-27 12:49:41 -0500

    better submitting

--- boolmininfo/slurm_submitter.py
+++ boolmininfo/slurm_submitter.py
@@ -2,17 +2,19 @@ import os
 import pandas as pd
 
 eca = pd.read_csv('data/eca_equiv_classes.csv', index_col=None)
-rules = eca['rule']
+rules = eca[eca['wclass'] == 4]['rule']
+submit_file = '/N/u/patgwall/BigRed3/boolmininfo/slurmy.script'
 
 out_str = """#!/bin/bash
 #####  Constructed by HPC everywhere #####
 #SBATCH --mail-user=patgwall@iu.edu
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --time=3-00:59:00
+#SBATCH --time=0-08:00:00
 #SBATCH --partition=general
 #SBATCH --mail-type=FAIL
 #SBATCH --job-name=eca_{0:d}
+#SBATCH --output=logs/eca_{1}
 
 ######  Module commands #####
 
@@ -20,8 +22,12 @@ out_str = """#!/bin/bash
 
 ######  Job commands go below this line #####
 cd ~/boolmininfo/
-python boolmininfo/eca/stat_dynamics.py {1:d}"""
+python boolmininfo/eca/stat_dynamics.py {2:d}
+rm {3}"""
+
 
 for rule in rules:
-    submit_str = out_str.format(rule, rule)
-    print(submit_str)
+    submit_str = out_str.format(rule, rule, rule, submit_file)
+    with open(submit_file, 'w') as fout:
+        fout.write(submit_str)
+    os.system('sbatch ' + submit_file)
